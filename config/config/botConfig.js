0f698bce256360de2ee67e7dbc801524db03e65b
⎯⃝♡︎ᴛꫝ֟፝ؖ۬ᴍɪᴍ⎯⃝々⸙ <reportking69@outlook.com>
2026-04-09 14:51:36 +0600

    Create botConfig.js

---
+++ config/config/botConfig.js
@@ -0,0 +1,5 @@
+module.exports = {
+  BOT_TOKEN: "8643206314:AAG4W1fqTepqktrE_xzxbn4KI9GY1x1X188",
+  DOWNLOAD_API: "https://xsaim8x-xxx-api.onrender.com/api/auto",
+  BASE_API_URL: "https://raw.githubusercontent.com/mahmudx7/HINATA/main/baseApiUrl.json"
+};
